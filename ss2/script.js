c3ec3fb69ecd6f85a951f4f2651afdffec95dbf8
tom <h>
2020-02-18 22:08:29 +0700

    ss2

---
+++ ss2/script.js
@@ -0,0 +1,55 @@
+// alert("xin chào c4ejs 71"); // thông báo
+
+// prompt("tên bạn là gì"); // nhập thông tin
+
+// let age; // khai báo biến
+// age=25; // gán giá tri
+// let age1=35
+
+// sử dung;
+// alert(age); // <=ok để chạy tiếp trang
+
+// alert(age1);
+// console.log(age1); // trang k bị chặn bởi khai báo let
+// console.log(age); // trang k bị chặn bởi khai báo let
+
+
+//  var name;// bien ten, chu ky tu
+//  const PI=3.14;
+//  let d = prompt("nhap bk hinh tron");
+//  let dientich=PI*d*d;
+//  let chuvi=PI*2*d;
+//  alert("dien tich =:");
+//  alert(dientich);
+//  alert("chu vi =:");
+//  alert(chuvi);
+
+//  let a=5;
+//  let b=10;
+
+//  let c = a+b;
+ 
+//  console.log(c);
+
+ 
+
+//  let yourName = prompt("nhập tên:");
+//  let yourAge = prompt("nhập tuổi:");
+//  let yourYearOfBirth = prompt("nhập năm sinh:");
+//  alert(yourName);
+//  alert(yourAge);
+//  yourAge=2020-yourYearOfBirth;
+//  alert(yourAge);
+
+alert("nhâp hai số:");
+let s1 = prompt("nhập số thứ nhất:");
+let s2 = prompt("nhập số thứ hai:");
+cong= s1 +s2;
+tru= s1 -s2;
+nhan= s1*s2;
+chia=s1/s2;
+alert(cong);// loi
+alert(tru);
+alert(nhan);
+alert(chia);
+ 
