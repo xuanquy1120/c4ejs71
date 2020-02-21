87d10ac72870019f7dd8d483e167c8c71971a0b8
tom <h>
2020-02-21 23:19:43 +0700

    ss3

---
+++ ss3_Datatype/script.js
@@ -0,0 +1,47 @@
+// let welcome = "xin chào";
+// let welcome1 = ` ${welcome} c4ejs 71`;
+// console.log(welcome);
+
+// let x=10;
+// let y=4*(x*x+10*x*Math.sqrt(x*x+3*x+1));
+// console.log(y);
+
+// let yourName="A";
+// alert("xin chào "+ yourName);
+
+// let x= Number("5");
+// let y=Number("6");
+// console.log(x+y);
+
+// // chuỗi => số nguyên
+// x=parseInt("5.6");
+// console.log((x));
+
+// //chuỗi thành số thực
+// y=parseFloat("6.51111");
+
+// // số => chuỗi
+// // số + ''
+// let s=10 + '';// => s="10";
+
+
+
+//  let d = parseFloat(prompt("nhap bk hinh tron"));
+//  let dientich=Math.PI*d*d;
+//  let chuvi=Math.PI*2*d;
+//  alert("dien tich =:");
+//  alert(dientich);
+//  alert("chu vi =:");
+//  alert(chuvi);
+
+let a=parseFloat(prompt("nhập chiều rộng:"));
+let b=parseFloat(prompt("nhập chiều dài:"));
+let p=2*(a+b);
+let s=a*b;
+console.log(`chu vi la ${p}`);
+console.log(`dien tich la ${s}`);
+
+
+console.log(typeof a);// kiểm tra kiểu dữ liệu
+
+
