c3ccc505d36d777fae2b2ac2c786922e92144447
tom <h>
2020-02-21 23:20:16 +0700

    ss4

---
+++ ss4_condition/script.js
@@ -0,0 +1,25 @@
+let number = Number(prompt("nhập 1 số"));
+let month = Number(prompt(""))
+// if (number>8){
+//     console.log("nó lớn hơn 8");
+// }
+// if(number<8){
+//     console.log("nó bé hơn 8");    
+// }
+// if(number===8){
+//     console.log("nó bằng 8");
+// }
+// if(number%2===0){
+//     console.log("số chẵn");
+// }
+// else {
+//     console.log("số lẻ");
+// }
+
+if(number%4===0&&number%100!==0){
+    console.log("năm nhuận");
+}
+else {
+    console.log("năm k nhuận");
+    
+}
