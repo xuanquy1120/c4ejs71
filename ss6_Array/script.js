beb26405ea72c1951136cae87a29593a85ce8df7
tom <h>
2020-03-03 20:15:36 +0700

    ss6

---
+++ ss6_Array/script.js
@@ -0,0 +1,100 @@
+// let arr =[1,2,3,4,5];
+
+// let listNumbers = [1,2,3,4,5];
+
+// let listNames = [`quang`, "duy", 'tung'];
+
+// console.log(arr);
+
+// console.log(listNumbers);
+
+// console.log(listNames);
+
+// arr.push(10);
+
+// console.dir(arr);
+// arr.push(11,12,13,14);
+// console.table(arr);
+// arr.unshift(20);
+// console.dir(arr);
+// arr.unshift(21,22,23);
+// console.dir(arr);
+
+// console.log(arr[1]);
+// console.log(arr[14]);
+// console.log(arr[-1]);
+
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+
+// }
+
+// arr[arr.length]=500;
+// console.log(arr);
+// arr.splice(1,2);
+// console.log(arr);
+// let a= arr.pop();
+// let b=arr.shift();
+// console.log(`a=${a},b=${b}`);
+// console.log(arr);
+
+// let arr = [];
+// let length = 5;
+// for (let i = 0; i < length; i++) {
+//   let n = parseFloat(prompt("nhap 1 so"));
+//   arr.push(n);
+// }
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(`${i + 1}:${arr[i]}`);
+// }
+// let s = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   s += arr[i];
+// }
+// let ttb = s / 2;
+// console.log(`ttb=${ttb}`);
+
+// let isExist = false;
+// let N = Number(prompt("nhap 1 so:"));
+// for (let i = 0; i < arr.length; i++) {
+//   const element = arr[i];
+//   if (element === N) {
+//     isExist = true;
+//     break;
+//   }
+// }
+// if (isExist) {
+//   console.log("co ton tai");
+// } else {
+//   console.log("khong ton tai");
+// }
+
+// for (let i = 0; i < arr.length; i++) {
+//   const element = arr[i];
+//   if (element < N) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// let stt=parseInt(prompt("vi tri can sua"))
+//  while( stt<1 || stt> arr.length){
+//    stt=parseInt(prompt("nhap dung vi tri:"));
+//  }
+//  arr[stt-1]=Number(prompt("nhap gia tri moi"));
+ 
+// arr.sort();//tăng dần
+// arr.reverse();//đảo ngược thứ tự
+
+// let arr=[5,7,3,2,9]
+// for (let i=0;i<arr.length-1;i++){
+//   for(let j=0;j<arr.length;j++){
+//     if(arr[j]<arr[i]){
+//       let temp=arr[j];
+//       arr[j]=arr[i];
+//       arr[i]=temp;
+//     }
+//   }
+// }
+// console.log(arr);
+
+
